5a87f8df6a1d13a071eddd4cb2905d2e18da8691
hyiromori <hyiromori@gmail.com>
2020-09-11 11:55:18 +0900

    api.js の実装

--- handlers/api.js
+++ handlers/api.js
@@ -1,18 +1,44 @@
 'use strict';
+const {SES} = require('aws-sdk')
+const ses = new SES({
+  apiVersion: '2010-12-01',
+  region: 'us-east-1',
+})
 
-module.exports.handler = async event => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+module.exports.handler = async (event) => {
+  try {
+    const {to, subject, body} = JSON.parse(event.body)
+
+    const params = {
+      Destination: {
+        ToAddresses: [to]
       },
-      null,
-      2
-    ),
-  };
+      Message: {
+        Subject: {
+          Charset: "UTF-8",
+          Data: subject
+        },
+        Body: {
+          Text: {
+            Charset: "UTF-8",
+            Data: body
+          }
+        }
+      },
+      Source: process.env.SES_SENDER_ADDRESS
+    }
+    console.info('Send Email:', JSON.stringify(params))
+    await ses.sendEmail(params).promise();
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+    return {
+      statusCode: 200,
+      body: JSON.stringify({message: 'Success'}, null, 2)
+    };
+  } catch (err) {
+    console.error(err)
+    return {
+      statusCode: 500,
+      body: JSON.stringify({message: 'Error'}, null, 2),
+    };
+  }
 };
